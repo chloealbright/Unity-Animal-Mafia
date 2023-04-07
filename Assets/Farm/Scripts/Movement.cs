dd8e1e16365b6bc3c0164ac2f50df434f092bf38
Annie-Mai <anniemai025@gmail.com>
2023-04-06 22:20:20 -0400

    initial commit

---
+++ Assets/Farm/Scripts/Movement.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed;
+
+    public Animator animator;
+
+    private Vector3 direction;
+
+    //get input from player
+    //apply movement to sprite
+    private void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        direction = new Vector3(horizontal, vertical);
+
+        AnimateMovement(direction);
+    }
+
+    private void FixedUpdate()
+    {
+        //move the player
+        this.transform.position += direction * speed * Time.deltaTime;
+    }
+
+    //this function handles talking with the animator
+    void AnimateMovement(Vector3 direction)
+    {
+        //check to see if the animator reference is set - check to see if the value of the animator is not null
+        if(animator != null)
+        {
+            //check if the sprite is moving (direction vector x and y would be 0 if we dont receive an input we are not moving)
+            if(direction.magnitude > 0)
+            {
+                animator.SetBool("isMoving", true);
+                animator.SetFloat("horizontal", direction.x);
+                animator.SetFloat("vertical", direction.y);
+            }
+            else
+            {
+                animator.SetBool("isMoving", false);
+            }
+        }
+    }
+}
