72e2d09398a5b82cddf8b3f23569fe0351623412
Annie-Mai <anniemai025@gmail.com>
2023-05-08 19:35:43 -0400

    dialogue system: checked with the inventory, everything should function properly

---
+++ Assets/Farm/Scripts/NPC/NPC.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NPC : MonoBehaviour
+{
+    public GameObject dialoguePanel;
+    public TMP_Text dialogueText;
+    public string[] dialogue;
+    private int index;
+
+    public GameObject contButton;
+    public float wordSpeed;
+    public bool playerIsClose;
+
+    public GameObject bubble;
+    void Update()
+    {
+        if (!dialoguePanel.activeInHierarchy && playerIsClose)
+        {
+            bubble.SetActive(true);
+        }
+        else
+            bubble.SetActive(false);
+        if (Input.GetKeyUp(KeyCode.E) && playerIsClose)
+        {
+            bubble.SetActive(false);
+            if (dialoguePanel.activeInHierarchy)
+            {
+                zeroText();
+                contButton.SetActive(false);
+                StopAllCoroutines();
+            }
+            else
+            {
+                dialoguePanel.SetActive(true);
+                StartCoroutine(Typing());
+            }
+        }
+        if (dialogueText.text == dialogue[index])
+        {
+            contButton.SetActive(true);
+        }
+
+    }
+
+    public void zeroText()
+    {
+        dialogueText.text = "";
+        index = 0;
+        dialoguePanel.SetActive(false);
+    }
+
+    IEnumerator Typing()
+    {
+        foreach (char letter in dialogue[index].ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(wordSpeed);
+        }
+    }
+
+    public void NextLine()
+    {
+
+        contButton.SetActive(false);
+
+        if (index < dialogue.Length - 1)
+        {
+            index++;
+            dialogueText.text = "";
+            StartCoroutine(Typing());
+        }
+        else
+        {
+            zeroText();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerIsClose = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerIsClose = false;
+            zeroText();
+        }
+    }
+}
