f3886c83e1f02d2ab2466febad9e8c5c084de2e6
Daniel Lin <lindanny9721@gmail.com>
2023-05-09 14:28:21 -0400

    Commented out the exittile script to fix error

--- Assets/Dungeon/Scripts/Procedural Dungeon Generation/ExitTile.cs
+++ Assets/Dungeon/Scripts/Procedural Dungeon Generation/ExitTile.cs
@@ -1,13 +1,13 @@
-using UnityEngine;
+// using UnityEngine;
 
-public class ExitTile : MonoBehaviour
-{
+// public class ExitTile : MonoBehaviour
+// {
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
-        {
-            dungeonGenerator.CorridorFirstGeneration();
-        }
-    }
-}
+//     private void OnTriggerEnter2D(Collider2D collision)
+//     {
+//         if (collision.CompareTag("Player"))
+//         {
+//             dungeonGenerator.CorridorFirstGeneration();
+//         }
+//     }
+// }
