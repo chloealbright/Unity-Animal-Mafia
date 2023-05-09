1ddc32805ec84277f8668dff869765b2cbebbd03
Daniel Lin <lindanny9721@gmail.com>
2023-05-09 14:26:45 -0400

    Change the sprite and shooting/fixed some collisions

--- Assets/Dungeon/Scripts/Procedural Dungeon Generation/ExitTile.cs
+++ Assets/Dungeon/Scripts/Procedural Dungeon Generation/ExitTile.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class ExitTile : MonoBehaviour
 {
-    public CorridorFirstDungeonGenerator dungeonGenerator;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
