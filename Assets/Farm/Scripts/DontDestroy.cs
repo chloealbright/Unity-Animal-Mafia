234a06d7dd7e14e57fb788835018e0c67f5a09f1
Shermol <shermol.peters01@gmail.com>
2023-05-11 21:14:22 -0400

    Stopped game items frrom appearing in seperate scenes

--- Assets/Farm/Scripts/DontDestroy.cs
+++ Assets/Farm/Scripts/DontDestroy.cs
@@ -1,12 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DontDestroy : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    //void Start()
+    //{
+    //    DontDestroyOnLoad(gameObject); 
+    //}
+
+    private Scene originalScene;
+
+    void Awake()
     {
-        DontDestroyOnLoad(gameObject); 
+        // Store the original scene when the game object is created
+        originalScene = gameObject.scene;
+
+        // Mark the game object as persistent
+        DontDestroyOnLoad(gameObject);
+
+        // Register a callback for the SceneManager.sceneUnloaded event
+        SceneManager.sceneUnloaded += OnSceneUnloaded;
+    }
+
+    void OnDestroy()
+    {
+        // Unregister the OnSceneUnloaded method as a delegate for the SceneManager.sceneUnloaded event
+        SceneManager.sceneUnloaded -= OnSceneUnloaded;
+    }
+
+    void OnSceneUnloaded(Scene scene)
+    {
+        // If the current scene is not the original scene, remove the game object from the scene
+        if (scene.buildIndex != originalScene.buildIndex)
+        {
+            SceneManager.MoveGameObjectToScene(gameObject, SceneManager.GetActiveScene());
+        }
     }
 }
