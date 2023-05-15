5f2c54d68edf0d14b55366143d4c5e1b197d3a87
Annie-Mai <anniemai025@gmail.com>
2023-05-14 22:20:14 -0400

    ui for health and functionality

--- Assets/Dungeon/Scripts/Procedural Dungeon Generation/AnimalHealth.cs
+++ Assets/Dungeon/Scripts/Procedural Dungeon Generation/AnimalHealth.cs
@@ -1,22 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AnimalHealth : MonoBehaviour
 {
-    [SerializeField] private int maxHealth = 2;
-    private int currentHealth;
+    [SerializeField] private float maxHealth = 100f;
+    private float currentHealth;
+
+    public Image healthBar;
+
+    public TMP_Text healthText;
 
     private void Start()
     {
         currentHealth = maxHealth;
+
+        GameObject imageObject = GameObject.FindGameObjectWithTag("HealthBar");
+        healthBar = imageObject.GetComponent<Image>();
+        GameObject textObject = GameObject.FindGameObjectWithTag("HealthValue");
+        healthText = textObject.GetComponent<TMP_Text>();
+        healthText.text = maxHealth.ToString();
     }
 
    void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("EnemyBullet"))
         {
-            currentHealth -= 1;
+            currentHealth -= 25;
+            healthBar.fillAmount = currentHealth/ maxHealth;
+            healthText.text = currentHealth.ToString();
             if (currentHealth <= 0)
             {
                 Die();
@@ -27,7 +41,7 @@ public class AnimalHealth : MonoBehaviour
 
     public void Heal(int amount)
     {
-        currentHealth = Mathf.Min(currentHealth + amount, maxHealth);
+        //currentHealth = Mathf.Min(currentHealth + amount, maxHealth);
     }
 
     private void Die()
