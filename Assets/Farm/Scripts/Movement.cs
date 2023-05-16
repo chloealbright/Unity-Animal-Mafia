f5623cfc9c590aabb3bdda03e358d33d87692d2e
Annie-Mai <anniemai025@gmail.com>
2023-05-16 05:35:03 -0400

    default position gets reseted after each play for testing purposes

--- Assets/Farm/Scripts/Movement.cs
+++ Assets/Farm/Scripts/Movement.cs
@@ -17,6 +17,7 @@ public class Movement : MonoBehaviour
 
     void Start()
     {
+        startingPosition.initialValue = startingPosition.defaultValue;
         transform.position = startingPosition.initialValue;
     }
 
