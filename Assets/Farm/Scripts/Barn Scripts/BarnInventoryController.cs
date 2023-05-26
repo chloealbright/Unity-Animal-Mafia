7b6f0bcf543a9cc25ce119a6e454989dd1a6d94b
Chloemz <57193454+Chloemz@users.noreply.github.com>
2023-05-25 22:14:33 -0400

    adding barn scripts back

---
+++ Assets/Farm/Scripts/Barn Scripts/BarnInventoryController.cs
@@ -0,0 +1,117 @@
+using InventoryCont.Model;
+using InventoryCont.UI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace InventoryCont
+{
+    public class BarnInventoryController : MonoBehaviour
+    {
+        //dependency to inv page
+        [SerializeField]
+        private InventoryPage inventoryUI;
+
+        [SerializeField]
+        private InventorySO inventoryData;
+        
+        [SerializeField]
+        private BalancePage balancePanel;
+
+        public GameObject player;
+        public List<InventoryItemStruct> initialItems = new List<InventoryItemStruct>();
+
+        private void Start()
+        {
+            PrepareUI();
+            PrepareInventoryData();
+        }
+
+        private void PrepareInventoryData()
+        {
+            inventoryData.Initialize();
+            inventoryData.OnInventoryUpdated += UpdateInventoryUI;
+            foreach (InventoryItemStruct item in initialItems)
+            {
+                if (item.isEmpty)
+                    continue;
+                inventoryData.AddItem(item);
+            }
+        }
+
+        //updates inventory when performing change
+        private void UpdateInventoryUI(Dictionary<int, InventoryItemStruct> inventoryState)
+        {
+            inventoryUI.ResetAllItems();
+            foreach (var item in inventoryState)
+            {
+                inventoryUI.UpdateData(item.Key, item.Value.item.ItemImage, item.Value.quantity);
+            }
+        }
+
+        private void PrepareUI()
+        {
+            inventoryUI.InitializeInventoryUI(inventoryData.Size);
+            inventoryUI.OnDescriptionRequested += HandleDescriptionRequest;
+            inventoryUI.OnSwapItems += HandleSwapItems;
+            inventoryUI.OnStartDragging += HandleDragging;
+            inventoryUI.OnItemActionRequested += HandleItemActionRequest;
+        }
+
+        private void HandleItemActionRequest(int itemIndex)
+        {
+
+        }
+
+        private void HandleDragging(int itemIndex)
+        {
+            InventoryItemStruct inventoryItemStruct = inventoryData.GetItemAt(itemIndex);
+            if (inventoryItemStruct.isEmpty)
+                return;
+            inventoryUI.CreateDraggedItem(inventoryItemStruct.item.ItemImage, inventoryItemStruct.quantity);
+        }
+
+        private void HandleSwapItems(int itemIndex1, int itemIndex2)
+        {
+            inventoryData.SwapItems(itemIndex1, itemIndex2);
+        }
+
+        private void HandleDescriptionRequest(int itemIndex)
+        {
+            InventoryItemStruct inventoryItemStruct = inventoryData.GetItemAt(itemIndex);
+            if (inventoryItemStruct.isEmpty)
+            {
+                inventoryUI.ResetSelection();
+                return;
+            }
+            ItemSO item = inventoryItemStruct.item;
+            inventoryUI.UpdateDescription(itemIndex, item.ItemImage, item.name, item.Description);
+        }
+
+        //user pressing I to open inv
+        public void Update()
+        {
+            if(Input.GetKeyDown(KeyCode.I))
+            {
+                if (inventoryUI.isActiveAndEnabled == false)
+                {
+                    inventoryUI.Show();
+                    foreach (var item in inventoryData.GetCurrentInventoryState())
+                    {
+                        inventoryUI.UpdateData(item.Key,
+                            item.Value.item.ItemImage,
+                            item.Value.quantity);
+                    }
+                    balancePanel.Show();
+                }
+                else
+                {
+                    inventoryUI.Hide();
+                    balancePanel.Hide();
+                }
+            }
+
+        }
+    }
+}
---
+++ Assets/Farm/Scripts/Barn Scripts/BarnSceneMove.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BarnSceneMove : MonoBehaviour
+{
+    public int sceneBuildIndex;
+    public Vector2 playerPosition;
+    public VectorValue playerStorage;
+    public GameObject fadeInPanel;
+    public GameObject fadeOutPanel;
+    public float fadeWait;
+
+    private void Awake()
+    {
+        if(fadeInPanel != null)
+        {
+            GameObject panel = Instantiate(fadeInPanel, Vector3.zero, Quaternion.identity) as GameObject;
+            Destroy(panel, 1);
+        }
+    }
+    //level move zoned enter, if collider is a player
+    //move game to another scene
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        print("Trigger Entered");
+        //can use other.GetComponent<Player>() to see if the game object has a player component
+        //tags work too
+        if (other.tag == "Player" && !other.isTrigger)
+        {
+            //player entered, so move level
+            BarnMovement movementScript = other.GetComponent<BarnMovement>();
+            //pausing player movement while in transition
+            movementScript.canMove = false;
+
+            playerStorage.initialValue = playerPosition;
+            StartCoroutine(FadeController());
+
+            movementScript.canMove = true;
+            //SceneManager.LoadScene(sceneBuildIndex);
+        }
+    }
+
+    public IEnumerator FadeController()
+    {
+        if(fadeOutPanel != null)
+        {
+            GameObject panel1 = Instantiate(fadeOutPanel, Vector3.zero, Quaternion.identity);
+            Destroy(panel1, 1);
+        }
+        yield return new WaitForSeconds(fadeWait);
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(sceneBuildIndex);
+        while(!asyncOperation.isDone)
+        {
+            yield return null;
+        }
+    }
+
+}
